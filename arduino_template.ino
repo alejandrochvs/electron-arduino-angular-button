23a4db02e715287146f75af8de9464f9ef96a6de
alejandro <alejandrochvs@hotmail.com>
2017-12-09 09:31:45 -0500

    V.1.0 ;

--- arduino_template.ino
+++ arduino_template.ino
@@ -1,26 +1,37 @@
-/*
-  AnalogReadSerial
+#include <Keypad.h>
 
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+const byte numRows= 4; //number of rows on the keypad
+const byte numCols= 4; //number of columns on the keypad
 
-  This example code is in the public domain.
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols]=
+{
+{'1', '2', '3', 'A'},
+{'4', '5', '6', 'B'},
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
 
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
-*/
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {9,8,7,6}; //Rows 0 to 3
+byte colPins[numCols]= {5,4,3,2}; //Columns 0 to 3
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+void setup()
+{
+Serial.begin(9600);
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(20);        // delay in between reads for stability
+//If key is pressed, this key is stored in 'keypressed' variable
+//If key is not equal to 'NO_KEY', then this key is printed out
+//if count=17, then count is reset back to 0 (this means no key is pressed during the whole keypad scan process
+void loop()
+{
+char keypressed = myKeypad.getKey();
+if (keypressed != NO_KEY)
+{
+Serial.println(keypressed);
+}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,36 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  encapsulation : ViewEncapsulation.None,
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  quantity = 0;
+export class AppComponent implements OnInit {
+  string = '';
 
   constructor() {
     const socket = io('http://localhost:3000');
     socket.on('data', (data) => {
-      this.quantity = Math.round(data / 10);
+      this.append(data);
     });
+  }
+
+  ngOnInit() {
+    const classes = document.getElementsByClassName('key');
+    Array.from(classes).forEach((element) => {
+      element.addEventListener('click', (event) => {
+        this.append(event.srcElement.innerHTML);
+
+      });
+    });
+  }
+
+  append(key) {
+    if (isNaN(key) || key === null) {
+      return this.string = this.string.slice(0, -1);
+    }
+    this.string += key;
   }
 }
